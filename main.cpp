4283e021539e93395d4a573a56eb9b932e07f125
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-10-02 17:58:33 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <libs/uwebsockets/uWS.h>
 // this will be filled out later
+
+int main(int argc, char *argv[]) {
+	// start
+	uWS::Hub websocketHub;
+	
+	websocketHub.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+        ws->send(message, length, opCode);
+    });
+
+    if (websocketHub.listen(argv[1])) { // argv[1] is port
+        websocketHub.run();
+    }
+	
+	//std::vector<std::unordered_map<std::string, auto>> rooms;// auto for now
+}
