ed24daa2c6f403ce4a42ced27a62e67d9d76ac18
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-10-04 14:46:30 -0400

    Update room.cpp

--- src/room.cpp
+++ src/room.cpp
@@ -1,5 +1,22 @@
+#include <libs\uws\uWS.h>
 #include room.h
 
-BallRoom::getNumOfPlayers() {
-  return hdhhd;//will change
+Room::getNumOfPlayers() {
+  return numOfPlayers;//will change
+}
+
+Room::getMaxNumOfPlayers() {
+  return maxNumOfPlayers;//will change
+}
+
+Room::addPlayer(uWS::WebSocket<uWS::CLIENT> websocket) {
+  std::unordered_map<std::string, auto> player = {
+    {"websocket", websocket}
+  };
+  numOfPlayers += 1;
+  players.push_back(player);
+}
+
+Room::isFull() {
+  return (getNumOfPlayers() <= getMaxNumOfPlayers());
 }
