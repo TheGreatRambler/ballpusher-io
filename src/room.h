07197422c349820cc88362781ed9b1112b8914ec
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-10-04 14:46:28 -0400

    Update room.h

--- src/room.h
+++ src/room.h
@@ -4,7 +4,10 @@ class Room {
   private:
     double getPercentFull();
     int getNumOfPlayers();
+    int numOfPlayers = 0;
+    int maxNumOfPlayers = 30;
     std::vector<std::unordered_map<std::string, auto>> players;
   public:
+    bool isFull();
     void addPlayer();
 }
